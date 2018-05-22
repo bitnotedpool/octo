5955862b4b668db1849932ef0a97a89e9a79adfd
bitnotedpool <36618000+bitnotedpool@users.noreply.github.com>
2018-05-22 18:25:42 +0200

    init
    test ubuntu

---
+++ external/rocksdb/util/build_version.cc
@@ -0,0 +1,4 @@
+#include "build_version.h"  
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:0";  
+const char* rocksdb_build_git_datetime = "rocksdb_build_git_datetime:05/11/18 13:36:45";  
+const char* rocksdb_build_compile_date = __DATE__; 
